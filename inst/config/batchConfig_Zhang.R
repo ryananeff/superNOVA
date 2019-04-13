c94bae8edc8a0f80079d3b39fc16993b231bad24
nosarcasm <raneff@gmail.com>
2019-04-13 12:42:35 -0400

    fixed config file issue

--- inst/config/batchConfig_Zhang.R
+++ inst/config/batchConfig_Zhang.R
@@ -1,8 +1,8 @@
-## For configuration instructions and examples, see the batchtools R package reference on CRAN. 
+## For configuration instructions and examples, see the batchtools R package reference on CRAN.
 
-cluster.functions = makeClusterFunctionsLSF(system.file("config/minerva_LSF.tmpl",package="DGCA")
+cluster.functions = makeClusterFunctionsLSF(system.file("config/minerva_LSF.tmpl",package="superNOVA")
                                             ,scheduler.latency = 1)
-libpath = system.file("/../",package="DGCA")
+libpath = system.file("/../",package="superNOVA")
 default.resources = list(queue="premium", walltime=60, memory=2000, cores=1, project="acc_zhangb03a",libpath = libpath)
 raise.warnings = FALSE
 staged.queries = TRUE
