6f03de6c3d025b2ac20252a7f4bc6457ae1b2fc7
nosarcasm <raneff@gmail.com>
2019-06-21 19:51:40 -0400

    added example to test that the main function is working (basic)

---
+++ examples/test_superNOVA.R
@@ -0,0 +1,18 @@
+rm(list = ls(all.names = TRUE))
+set.seed(12345)
+
+library("DGCA")
+library("superNOVA")
+
+options(stringsAsFactors = FALSE)
+
+message("loading data")
+message(Sys.time())
+
+data(darmanis) #loads daramanis
+data(design_mat) #loads design_mat
+
+rownames(design_mat) = colnames(darmanis)
+
+chow_res = chowCor(matA = darmanis, design_mat=design_mat,compare = c("oligodendrocyte", "neuron"))
+ddcor_res = flattenChow(chow_res)
