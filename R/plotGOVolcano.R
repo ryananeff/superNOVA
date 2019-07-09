e4a899263b0716df8f00a854f84ed3c1a1b3d3f9
nosarcasm <raneff@gmail.com>
2019-07-09 14:29:53 -0400

    more changes to plotting function to output PDFs now

--- R/plotGOVolcano.R
+++ R/plotGOVolcano.R
@@ -1,4 +1,6 @@
-plotGOVolcano <- function(supergo_file, diffexp_file, gene_cutoff = 0.05, num_plots=10,num_genes=20){
+plotGOVolcano <- function(supergo_file, diffexp_file, gene_cutoff = 0.05, pathway_cutoff = 0.05,
+                          num_plots=10,num_genes=20,
+                          prefix="plotGO_",plt_size=5){
 
   options(stringsAsFactors = F)
   options(warn = 0)
@@ -7,14 +9,15 @@ plotGOVolcano <- function(supergo_file, diffexp_file, gene_cutoff = 0.05, num_pl
 
   diffexp = read.table(diffexp_file,header = T,sep="\t")
   rownames(diffexp) = make.names(diffexp[,1],unique = T) #gene names should be in the first column and should be unique
-  diffexp[,"logP"] = -1*log(as.numeric(diffexp$P.Value))
+  diffexp[,"logP"] = -1*log(as.numeric(diffexp$P.Value),base=10)
 
   supergo_res = read.table(supergo_file,header=T,sep="\t")
-  supergo_res[,"logP"] = sapply(-1*log(as.numeric(supergo_res$pVal)),FUN=function(x){min(x,50)})
+  supergo_res[,"logP"] = sapply(-1*log(as.numeric(supergo_res$pVal),base=10),FUN=function(x){min(x,20)})
   supergo_res = supergo_res[order(supergo_res[,"logP"],decreasing = T),]
 
   for (ix in 1:num_plots){
     top_mod = supergo_res[ix,]
+    if (top_mod$pVal > pathway_cutoff){ next }
     name = top_mod$Module
 
     gene_changecor_up = sapply(unlist(strsplit(as.character(top_mod$Top_GOC),split = '; ')),
@@ -31,7 +34,7 @@ plotGOVolcano <- function(supergo_file, diffexp_file, gene_cutoff = 0.05, num_pl
     gene_changecor = gene_changecor[unique(names(gene_changecor))]
 
     gene_pval = sapply(unlist(strsplit(as.character(top_mod$gene_pVal),split = '; ')),
-                  FUN=function(x){min(-1*log(as.numeric(unlist(strsplit(as.character(x),split=':'))[2])),12)},USE.NAMES = F)
+                  FUN=function(x){min(-1*log(as.numeric(unlist(strsplit(as.character(x),split=':'))[2]),base=10),7)},USE.NAMES = F)
     names(gene_pval) = sapply(unlist(strsplit(as.character(top_mod$gene_pVal),split = '; ')),
                               FUN=function(x){unlist(strsplit(as.character(x),split=':'))[1]},USE.NAMES = F)
 
@@ -43,21 +46,24 @@ plotGOVolcano <- function(supergo_file, diffexp_file, gene_cutoff = 0.05, num_pl
 
     data = data.frame(x=gene_changecor,y=gene_pval,c=diffexp_row,s=sizes,name=genes)
 
+    pdf(paste0(prefix,name,".pdf"),width=plt_size*1.2,height=plt_size)
     g = ggplot(data,aes(x=x,y=y,color=c,size=s)) +
-          xlim(-0.5,0.5) + ylim(-0.3,15) +
+          xlim(-0.5,0.5) + ylim(-0.3,10) +
           geom_point() +
           scale_color_gradient2(low="blue",high="red",mid="white") +
-          geom_text_repel(data = subset(data[order(gene_pval,decreasing = T)[1:num_genes],],y>-1*log(gene_cutoff)),
+          geom_text_repel(data = subset(data[order(gene_pval,decreasing = T)[1:num_genes],],y>-1*log(gene_cutoff,base=10)),
                           aes(label=name),
                           color="brown",
                           box.padding=0.7,
                           point.padding=0.2,
                           segment.color="gray") +
-          theme(text=element_text(size=14)) +
+          theme(text=element_text(size=14),plot.title=element_text(size=10)) +
           labs(title=name,x="Mean correlation change",y="Gene level -logP",size="DEG -logP",color="DEG logFC") +
-          geom_hline(yintercept=-1*log(gene_cutoff),linetype="dashed",color="red") +
+          geom_hline(yintercept=-1*log(gene_cutoff,base=10),linetype="dashed",color="red") +
           geom_vline(xintercept=0,linetype="dashed",color="red")
     print(g)
-    invisible(readline(prompt="Press [enter] to continue"))
+    dev.off()
+    message(paste("Plotting",name,"..."))
+    #invisible(readline(prompt="Press [enter] to continue"))
   }
 }
